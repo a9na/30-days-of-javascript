3178f29d117cccfd60723a5e642ce0e5e05fc26b
Ana Novković <anaiscoding@gmail.com>
2024-08-16 20:47:15 +0200

    Update day12.js

--- day12.js
+++ day12.js
@@ -1,7 +1,6 @@
 /*
-2723. Add Two Promises
-Easy
-Companies
+Add Two Promises
+
 Given two promises promise1 and promise2, return a new promise. promise1 and promise2 will both resolve with a number. The returned promise should resolve with the sum of the two numbers.
  
 
