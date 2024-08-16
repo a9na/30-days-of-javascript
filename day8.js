aac427b3b6663c5ba04dff83ebac362180eb69d5
Ana Novković <anaiscoding@gmail.com>
2024-08-16 20:50:05 +0200

    Update day8.js

--- day8.js
+++ day8.js
@@ -1,7 +1,6 @@
-/* Function Composition
-Easy
-Companies
-Hint
+/* 
+Function Composition
+
 Given an array of functions [f1, f2, f3, ..., fn], return a new function fn that is the function composition of the array of functions.
 
 The function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))).
