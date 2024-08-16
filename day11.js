83e2753196d046dbab7103756b29d90dcb3b6137
Ana Novković <anaiscoding@gmail.com>
2024-08-16 20:46:57 +0200

    Update day11.js

--- day11.js
+++ day11.js
@@ -1,8 +1,6 @@
 /*
-2623. Memoize
-Medium
-Companies
-Hint
+Memoize
+
 Given a function fn, return a memoized version of that function.
 
 A memoized function is a function that will never be called twice with the same inputs. Instead it will return a cached value.
