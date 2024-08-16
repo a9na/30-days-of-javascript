1d46978d0d092423529e6c8767e5e8e7075ab7fe
Ana Novković <anaiscoding@gmail.com>
2024-08-16 20:48:56 +0200

    Update day6.js

--- day6.js
+++ day6.js
@@ -1,7 +1,6 @@
-/* Filter Elements from Array
-Easy
-Companies
-Hint
+/* 
+Filter Elements from Array
+
 Given an integer array arr and a filtering function fn, return a filtered array filteredArr.
 
 The fn function takes one or two arguments:
